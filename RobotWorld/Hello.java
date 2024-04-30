55229c0df7a06c22a86bca0c5430c24f1d3b597b
Your Name <you@example.com>
2024-04-30 18:22:19 +0200

    Testing-123

--- RobotWorld/Hello.java
+++
@@ -1,7 +0,0 @@
-// Test Program
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ RobotWorld/UserInput.java
@@ -0,0 +1,17 @@
+// Test Program
+import java.util.Scanner;
+class UserInput {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("enter your name");
+        String name = scan.nextLine();
+        System.out.println("Enter your age:");
+        int age = scan.nextInt();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + Age);
+
+    }
+}
